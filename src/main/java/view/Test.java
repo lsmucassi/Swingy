56b8b12e0854846bf4908ade57829c46a72fe8bb
Linda MUCASSI <lmucassi@e5r6p6.wethinkcode.co.za>
2018-07-10 15:46:51 +0200

    :(

---
+++ src/main/java/view/Test.java
@@ -0,0 +1,19 @@
+package view;
+
+import javax.swing.*;
+
+public class Test {
+    private JTextPane _displayPanel;
+    private JPanel panel1;
+    private JButton upBtn;
+    private JButton downBtn;
+    private JButton leftBtn;
+    private JButton rightBtn;
+    private JButton okBtn;
+    private JButton button1;
+    ;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
